a79d87d68d6f03cbc0b534f1fdbf689d3b69f792
RomaVintonyak <19roman23@gmail.com>
2022-01-03 13:36:52 +0200

    create about section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 jQuery(document).ready(function () {
   "use script";
+  //box slider
+  $('#introSlider').boxSlider({
+    effect: 'scrollHorz',
+    /*blindCount: 20,*/
+    pauseOnHover: true,
+    responsive: true,
+    speed: 1800,
+    timeout: 3000,
+    perspective: 1000,
+    autoScroll: true
+  });
 
 });
