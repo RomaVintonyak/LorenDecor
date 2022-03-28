9d718395451ac7dca2e2cf2486de1bb6f53d977a
RomaVintonyak <19roman23@gmail.com>
2022-03-28 17:57:07 +0300

    add contact form verification script

--- app/js_src/main.js
+++ app/js_src/main.js
@@ -108,7 +108,102 @@ jQuery(document).ready(function () {
   );
   jumpCard.mouseleave(function () {
     jumpCard.removeAttr("style");
-  }); 
+  });
+  //active style to label in contact form
+  var nameField = $("#name");
+  nameField.blur( function(){
+    var nameFieldData = $(this).val();
+    if(nameFieldData.length >= 1){
+      $("#lebelName").css({
+        "font-size": "1.2rem",
+        "top": "-2.7rem"
+      });
+    }else{
+      $("#lebelName").removeAttr("style");
+    }
+  });
+  var emailField = $("#email");
+  emailField.blur( function(){
+    var emailFieldData = $(this).val();
+    if(emailFieldData.length >= 1){
+      $("#labelEmail").css({
+        "font-size": "1.2rem",
+        "top": "-2.7rem"
+      });
+    }else{
+      $("#labelEmail").removeAttr("style");
+    }
+  });
+  var phoneField = $("#phone");
+  phoneField.blur( function(){
+    var phoneFieldData = $(this).val();
+    if(phoneFieldData.length >= 1){
+      $("#labelPhone").css({
+        "font-size": "1.2rem",
+        "top": "-2.7rem"
+      });
+    }else{
+      $("#labelPhone").removeAttr("style");
+    }
+  });
+  var messageField = $("#message");
+  messageField.blur( function(){
+    var messageFieldData = $(this).val();
+    if(messageFieldData.length >= 1){
+      $("#labelMessage").css({
+        "font-size": "1.2rem",
+        "top": "-3.5rem"
+      });
+    }else{
+      $("#labelMessage").removeAttr("style");
+    }
+  });
+  //contact page form validation
+  var mailBtn = $("#sendMail");
+  mailBtn.on("click", function () {
+    var name = $("#name").val().trim();
+    var phone = $("#phone").val().trim(),
+      intRegex = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+    var email = $("#email").val().trim(),
+      emailReg = /^[a-z0-9_-]+@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i;
+    var message = $("#message").val().trim();
+    if (name.length < 3) {
+      var nameError = $("#errorName").text();
+      $("#errorText").text(nameError);
+      return false;
+    } else if (!emailReg.test(email) || email == '') {
+      var mailError = $("#errorEmail").text();
+      $("#errorText").text(mailError);
+      return false;
+    } else if ((phone.length < 6) || (!intRegex.test(phone))) {
+      var phoneError = $("#errorPhone").text();
+      $("#errorText").text(phoneError);
+      return false;
+    } else if (message.length < 10) {
+      var messageError = $("#errorMessage").text();
+      $("#errorText").text(messageError);
+      return false;
+    }
+    $("#errorText").text("");
+    $.ajax({
+      url: '../php/mail.php',
+      type: 'POST',
+      cache: false,
+      data: { 'name': name, 'phone': phone, 'email': email, 'message': message },
+      dataType: 'html',
+      beforeSend: function () {
+        mailBtn.prop("disabled", true);
+      },
+      success: function (data) {
+        if (!data)
+          alert("Щось не так ... Спробуйте ще раз!");
+        else
+          $("#contactForm").trigger("reset");
+        alert(data);
+        mailBtn.prop("disabled", false);
+      }
+    });
+  });
   //waves effect
   Waves.attach(".waves");
   Waves.init();
@@ -209,23 +304,23 @@ jQuery(document).ready(function () {
   //simple lightbox gallery page option
   var galleryPage = $(".box__gallery a");
   galleryPage.simpleLightbox({
-    disableRightClick:  true,
-    fadeSpeed:          400,
-    animationSpeed:     350,
-    docClose:           false,
-    nav:                true,
-    navText:            ['&larr;','&rarr;'],
-    captions:           true,
-    captionDelay:       200,
-    captionSelector:    'img',
-    captionType:        'attr',
-    captionPosition:    'bottom',
-    captionClass:       'gallery__img--title',
-    enableKeyboard:     true,
-    alertError:         true,
-    alertErrorMessage:  'Image not found, next image will be loaded',
+    disableRightClick: true,
+    fadeSpeed: 400,
+    animationSpeed: 350,
+    docClose: false,
+    nav: true,
+    navText: ['&larr;', '&rarr;'],
+    captions: true,
+    captionDelay: 200,
+    captionSelector: 'img',
+    captionType: 'attr',
+    captionPosition: 'bottom',
+    captionClass: 'gallery__img--title',
+    enableKeyboard: true,
+    alertError: true,
+    alertErrorMessage: 'Image not found, next image will be loaded',
   });
-  console.log(galleryPage);  
+  console.log(galleryPage);
   //gallery slider otions
   var gSlider = $("#gallerySlider");
   gSlider.slick({
