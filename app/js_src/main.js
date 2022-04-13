043a284274b9046b96c275c64198a256493c13c8
RomaVintonyak <19roman23@gmail.com>
2022-04-13 17:56:05 +0300

    add copy protect script

--- app/js_src/main.js
+++ app/js_src/main.js
@@ -381,4 +381,21 @@ jQuery(document).ready(function () {
       },
     ],
   });
+  /**Copy protection*/
+  document.ondragstart = noselect;
+  document.onselectstart = noselect;
+  document.oncontextmenu = noselect;
+  function noselect() { return false; }
+  $("img, i, h1, h2, h3, h4, h5, h6, p, span").dblclick(function () {
+      $(".copy_protect").addClass("show_mesag");
+      setTimeout(function () {
+          $(".copy_protect").removeClass("show_mesag");
+      }, 1500);
+  });
+  $("img, i").on("contextmenu", function () {
+      $(".copy_protect").addClass("show_mesag");
+      setTimeout(function () {
+          $(".copy_protect").removeClass("show_mesag");
+      }, 1500);
+  });
 });
